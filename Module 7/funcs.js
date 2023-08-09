d3ebcca3cfe3f68124ecf759c4df823fac6139eb
brhn-4 <71796616+brhn-4@users.noreply.github.com>
2023-08-08 22:17:19 -0400

    Create funcs.js

---
+++ Module 7/funcs.js
@@ -0,0 +1,14 @@
+function foo(){
+    alert('Hello World! \nWelcome to js')
+    
+}
+
+function updateClock(){
+    var now = new Date();
+    
+
+    // Format time as 2-digit values, prepending with 0 if necessary
+   
+}
+
+setInterval(updateClock, 1000); //Here is the auto call every 1 second for your clock function 
