d98f2297a8e1406660958a7964cd625ce67b1bed
brhn-4 <71796616+brhn-4@users.noreply.github.com>
2023-08-09 22:30:51 -0400

    Create app.py

---
+++ Module 7/app.py
@@ -0,0 +1,25 @@
+########################################################################
+# Filename    : app.py
+# Description : python file to gather sensor data and return as JSON 
+#               object to web server
+# Author      : brhn
+# modification: 8/9/2023
+########################################################################
+
+
+
+
+
+
+
+
+# @app.route('/get_data', methods=['GET'])
+# def get_data():
+   
+
+#     return jsonify({
+        
+#     })
+
+if __name__ == '__main__':
+    app.run(port=5000)
