0f1537b05324cb1d4b790bdb3304144e4b8589ce
brhn-4 <71796616+brhn-4@users.noreply.github.com>
2023-08-19 14:24:54 -0400

    Update app.py

--- Module 8/SolutionFile/app.py
+++ Module 8/SolutionFile/app.py
@@ -2,8 +2,9 @@
 # Filename    : app.py
 # Description : sensor fetch function. pass in data to DOM in index.html
 # Author      : brhn
-# modification: 8/11/2023
+# modification: 8/19/2023
 ########################################################################
+
 
 
 
@@ -23,8 +24,8 @@ CORS(app)  # This will enable CORS for all routes
 
 def sensor(): #need array for the return serial numbers
     ret = []
-    for i in os.listdir('/sys/bus/w1/devices'):
-        if i != 'w1_bus_master1':
+    for i in os.listdir('/sys/bus/w1/devices'): #loop through dir
+        if i != 'w1_bus_master1': #if != 'w1_bus_master1' then i == serialNum. used in read()
             ret.append(i)
     return ret
 
@@ -43,17 +44,17 @@ def read(ds18b20): #multiple sensor serial nums
         data.append(farenheit)
     return data
 
-@app.route('/get_data', methods=['GET'])
-def get_data():
-    ds18b20 = sensor()
+@app.route('/get_data', methods=['GET']) #get route
+def get_data(): #get fcn
+    ds18b20 = sensor() #call sensor fcn from earlier module to read in water and air temps
     temperatures = read(ds18b20)
-    humidity, _ = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    humidity, _ = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN) #call sensor fcn from earlier module to read in humidity
 
-    return jsonify({
+    return jsonify({ #return json obj
         'air_temperature':'%0.2f F' % temperatures[0],
         'water_temperature': '%0.2f F' % temperatures[1],
         'humidity': '%0.2f ' % humidity + '%'
     })
 
-if __name__ == '__main__':
-    app.run(port=5000)
+if __name__ == '__main__': #set host and port (0.0.0.0 allows multiple viewers)
+    app.run(host='0.0.0.0',port=5000)
